ba2d161c7d77c71763ff40c9563ee0dcb0c03e84
Channing Morris <chan@mefightclub.com>
2017-06-05 15:08:03 -0500

    Initial commit.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMathLibrary
+{
+    public class Class1
+    {
+        public static int addSeven(int input)
+        {
+            return input + 7;
+        }
+    }
+}
